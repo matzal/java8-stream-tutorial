385fc2182d8f87085093b5b144cb2324954d31c9
matzal <mati.zalewski@gmail.com>
2018-03-29 17:03:03 +0200

    test method elementsStartWith - no such element

--- src/test/java/com/matzal/java8/StringStreamMethodsTest.java
+++ src/test/java/com/matzal/java8/StringStreamMethodsTest.java
@@ -18,4 +18,17 @@ class StringStreamMethodsTest {
         //then
         assertThat(filteredList).containsExactly("Andrzej", "Anna");
     }
+
+    @Test
+    public void elementsStartWithNoSuchElementTest() {
+        //given
+        List<String> myList = Arrays.asList("Jan", "Tomek", "Anna", "Alicja", "Andrzej");
+        StringStreamMethods stringStreamMethods = new StringStreamMethods();
+        //when
+        List<String> filteredList = stringStreamMethods.elementsStartWith(myList, "Z");
+        //then
+        assertThat(filteredList).isEmpty();
+    }
+
+
 }
