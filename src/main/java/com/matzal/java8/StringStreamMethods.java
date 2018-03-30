2a8c0b45af484ce3c58fa559df6429b53e328360
matzal <mati.zalewski@gmail.com>
2018-03-30 19:25:06 +0200

    test method finding max from string numbers

--- src/main/java/com/matzal/java8/StringStreamMethods.java
+++ src/main/java/com/matzal/java8/StringStreamMethods.java
@@ -1,15 +1,28 @@
 package com.matzal.java8;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class StringStreamMethods {
 
-    public List<String> elementsStartWith(List<String> inputList, final String beginningOfString){
+    public List<String> elementsStartWith(List<String> inputList, final String beginningOfString) {
         return inputList
                 .stream()
+                .filter(Objects::nonNull)
                 .filter(s -> s.startsWith(beginningOfString))
                 .sorted()
                 .collect(Collectors.toList());
+    }
+
+    public Integer parseToIntAndFindMax(List<String> stringList) {
+        return stringList
+                .stream()
+                .filter(Objects::nonNull)
+                .filter(s -> s.matches("^[0-9]*$"))
+                .mapToInt(Integer::parseInt)
+                .max()
+                .orElseThrow(NoSuchElementException::new);
     }
 }
--- src/test/java/com/matzal/java8/StringStreamMethodsTest.java
+++ src/test/java/com/matzal/java8/StringStreamMethodsTest.java
@@ -1,18 +1,28 @@
 package com.matzal.java8;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+        import org.junit.jupiter.api.Assertions;
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
+        import static org.assertj.core.api.Assertions.*;
+
+        import java.util.Arrays;
+        import java.util.List;
+        import java.util.NoSuchElementException;
 
 class StringStreamMethodsTest {
+
+    private StringStreamMethods stringStreamMethods;
+
+    @BeforeEach
+    void setUp() {
+        stringStreamMethods = new StringStreamMethods();
+    }
 
     @Test
     public void elementsStartWithTest() {
         //given
         List<String> myList = Arrays.asList("Jan", "Tomek", "Anna", "Alicja", "Andrzej");
-        StringStreamMethods stringStreamMethods = new StringStreamMethods();
         //when
         List<String> filteredList = stringStreamMethods.elementsStartWith(myList, "An");
         //then
@@ -30,5 +40,32 @@ class StringStreamMethodsTest {
         assertThat(filteredList).isEmpty();
     }
 
+    @Test
+    void parseToIntAndFindMaxTest() {
+        //given
+        List<String> stringNumbers = Arrays.asList("4", "8", "123", "32");
+        //when
+        Integer maxIntOfStringNumbers = stringStreamMethods.parseToIntAndFindMax(stringNumbers);
+        //then
+        assertThat(maxIntOfStringNumbers).isEqualTo(123);
+    }
 
+    @Test
+    void parseToIntAndFindMax_ListWithNull() {
+        //given
+        List<String> stringNumbers = Arrays.asList("4", "8", null, "32");
+        //when
+        Integer maxIntOfStringNumbers = stringStreamMethods.parseToIntAndFindMax(stringNumbers);
+        //then
+        assertThat(maxIntOfStringNumbers).isEqualTo(32);
+    }
+
+    @Test
+    void parseToIntAndFindMax_ListWithLetters() {
+        //given
+        List<String> stringNumbers = Arrays.asList("a", "b", "c", "d");
+        //when
+        //then
+        Assertions.assertThrows(NoSuchElementException.class, () -> stringStreamMethods.parseToIntAndFindMax(stringNumbers));
+    }
 }
