7db2f9a1e7ddbe6ba597f5481a462fdde64cd9d8
matzal <mati.zalewski@gmail.com>
2018-04-04 21:17:17 +0200

    create and test methods for persons

---
+++ src/main/java/com/matzal/java8/personExamples/PersonStreamMethods.java
@@ -0,0 +1,25 @@
+package com.matzal.java8.personExamples;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class PersonStreamMethods {
+
+    List<Person> getPersonsWithNameStarting(String nameStart, List<Person> inputList) {
+        return inputList.stream()
+                .filter(Objects::nonNull)
+                .filter(p -> p.getName()
+                        .startsWith(StringUtils.capitalize(nameStart)))
+                .collect(Collectors.toList());
+    }
+
+    Map<Integer, List<Person>> getPersonsByAge(List<Person> personList) {
+        return personList.stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.groupingBy(Person::getAge));
+    }
+}
---
+++ src/test/java/com/matzal/java8/personExamples/PersonStreamMethodsTest.java
@@ -0,0 +1,73 @@
+package com.matzal.java8.personExamples;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class PersonStreamMethodsTest {
+
+    PersonStreamMethods personStreamMethods = new PersonStreamMethods();
+
+    private final Person MAX = new Person("Max", 18);
+    private final Person PETER = new Person("Peter", 23);
+    private final Person PAMELA = new Person("Pamela", 23);
+    private final Person DAVID = new Person("David", 12);
+    private final Person PATRICK = new Person("Patrick", 40);
+
+    private List<Person> samplePersons =
+            Arrays.asList(MAX, PETER, PAMELA, DAVID, PATRICK);
+
+    @Test
+    void getPersonsWithNameStartingTest() {
+        //given
+        //when
+        List<Person> filteredPersonsList = personStreamMethods
+                .getPersonsWithNameStarting("pa", samplePersons);
+        //then
+        assertThat(filteredPersonsList).containsExactly(PAMELA, PATRICK);
+
+    }
+
+    @Test
+    void getPersonsWithNameStartingTest_ListWithNull() {
+        //given
+        List<Person> personListWithNull = new ArrayList<>(samplePersons);
+        personListWithNull.add(null);
+        //when
+        List<Person> filteredPersonsList = personStreamMethods
+                .getPersonsWithNameStarting("p", personListWithNull);
+        //then
+        assertThat(filteredPersonsList).containsExactly(PETER, PAMELA, PATRICK);
+
+    }
+
+    @Test
+    void getPersonsByAgeTest() {
+        //given
+        //when
+        Map<Integer, List<Person>> personsByAge = personStreamMethods
+                .getPersonsByAge(samplePersons);
+        //then
+        assertThat(personsByAge).containsKeys(12, 18, 23, 40)
+                .containsValues(Arrays.asList(MAX),
+                        Arrays.asList(DAVID),
+                        Arrays.asList(PETER, PAMELA),
+                        Arrays.asList(PATRICK));
+    }
+
+    @Test
+    void getPersonsByAgeTest_checkEntry() {
+        //given
+        //when
+        List<Person> personsWithAge12 = personStreamMethods
+                .getPersonsByAge(samplePersons)
+                .get(12);
+        //then
+        assertThat(personsWithAge12).containsExactly(DAVID);
+    }
+}
